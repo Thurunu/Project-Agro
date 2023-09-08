fd703c5647db3899d71b152aa62c660831abdf56
MTM Dilshan <86581450+Thurunu@users.noreply.github.com>
2023-09-08 21:47:01 +0530

    Add a sign out button

--- lib/Body/home_screen.dart
+++ lib/Body/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project_algora_2/Body/read_data/get_crop_details.dart';
 import '../widgets/background_circle.dart';
@@ -13,24 +14,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late String imageUrl;
 
-
   //document IDs
   final List<String> docIDs = [];
 
-
   //getDoc IDs
   Future<void> getDocIDs() async {
-
     await FirebaseFirestore.instance.collection("crop_details").get().then(
-          (snapshot) => snapshot.docs.forEach((document) {
-
-            docIDs.add(document.id);
-
-          },
+          (snapshot) => snapshot.docs.forEach(
+            (document) {
+              docIDs.add(document.id);
+            },
           ),
         );
   }
 
+  // Sign out function
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +44,6 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Stack(
           children: [
-
             Align(
               alignment: Alignment.topRight,
               child: BackgroundCircle(height: 300.0, width: 300.0),
@@ -49,33 +53,33 @@ class _HomeScreenState extends State<HomeScreen> {
               child: BackgroundCircle(height: 300.0, width: 300.0),
             ),
             const Padding(
-              padding: EdgeInsets.only(top:50, left: 20),
-              child: Text("Popular crops in your area",
+              padding: EdgeInsets.only(top: 50, left: 20),
+              child: Text(
+                "Popular crops in your area",
                 style: TextStyle(
                   color: Color.fromRGBO(27, 94, 32, 0.9),
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-
+            ElevatedButton(onPressed: _signOut, child: Text("Sign Out")),
             FutureBuilder(
-              future: getDocIDs(),
-                builder: (context, snapshot){
-
-                return Padding(
-                  padding: const EdgeInsets.only(top: 100),
-                  child: ListView.builder(
-                    itemCount: docIDs.length,
-                      itemBuilder: (context, index){
-                      return GetCropDetails(doucmentID: docIDs[index],width: currentWidth,);
-                      }
-                  ),
-                );
-                }
-            ),
+                future: getDocIDs(),
+                builder: (context, snapshot) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 100),
+                    child: ListView.builder(
+                        itemCount: docIDs.length,
+                        itemBuilder: (context, index) {
+                          return GetCropDetails(
+                            doucmentID: docIDs[index],
+                            width: currentWidth,
+                          );
+                        }),
+                  );
+                }),
           ],
-
         ),
       ),
     );
