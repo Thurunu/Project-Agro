661529657236c44b3c489682c48c073ac6dbce16
MTM Dilshan <86581450+Thurunu@users.noreply.github.com>
2023-09-09 10:20:17 +0530

    Navigation Bar Update
    Now When select items in navigation bar pages are change.

--- lib/Body/bottom_nav_bar_screen.dart
+++ lib/Body/bottom_nav_bar_screen.dart
@@ -1,45 +1,58 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'Pages/iot_page.dart';
 import 'Pages/plant_page.dart';
 import 'Pages/profile_page.dart';
-import 'home_screen.dart';
+import 'Pages/home_page.dart';
 
 class BottomNavBarScreen extends StatefulWidget {
-  const BottomNavBarScreen({super.key});
+  const BottomNavBarScreen({Key? key}) : super(key: key);
 
   @override
   State<BottomNavBarScreen> createState() => _BottomNavBarScreenState();
 }
 
 class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
-  int _currentIndex = 0;
-  final List<Widget> _pages = [
-    HomeScreen(),
-    PlantPage(),
-    IOTPage(),
-    ProfilePage(),
-  ];
+  int _page = 3;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: const Color.fromRGBO(52, 168, 83, 0.05),
-        animationDuration: const Duration(milliseconds: 500),
-        onTap: (index) {
+      body: PageView(
+        controller: _pageController,
+        children: const <Widget>[
+          HomeScreen(),
+          PlantPage(),
+          IOTPage(),
+          ProfilePage(),
+        ],
+        onPageChanged: (int index) {
           setState(() {
-            _currentIndex = index;
+            _pageController.jumpToPage(index);
           });
         },
-        items: const [
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _page,
+        items: const <Widget>[
           Icon(Icons.home),
           FaIcon(FontAwesomeIcons.leaf),
           FaIcon(FontAwesomeIcons.microchip),
           Icon(Icons.person),
         ],
+        onTap: (int index) {
+          setState(() {
+            _pageController.jumpToPage(index);
+          });
+        },
       ),
     );
   }
